e07d28edadbaa6254afd9e3a5b70bf89cef410ce
Poojethaa Kumar <poojethaa.kumar2@mindtree.com>
2021-01-06 19:07:34 +0530

    added some content

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This is README file
 Practicing merge conflict
+Added for testing conflict
---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('btn').addEventListener('click',()=>{
+    let name=document.getElementById("name").value;
+    alert(name);
+})
